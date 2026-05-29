bd7b887f717d6b4be6dcb265bbfb6b3241ef092f
krishnadevopstraining-tech <krishna.devopstraining@gmail.com>
2026-05-29 21:55:43 +0530

    Update Kubernetes installation commands in script

--- k8s-prereqs.sh
+++ k8s-prereqs.sh
@@ -79,9 +79,11 @@ sudo systemctl enable containerd
 # --------------------------------------------
 
 echo "Installing kubeadm, kubelet, kubectl..."
-curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | \
+sudo gpg --dearmor --yes -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /" | \
+sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update -y
 sudo apt install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
